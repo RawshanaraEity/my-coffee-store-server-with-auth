4a50e1fc1e32586022fb7d5ac88f5e1214f329f2
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-10-16 20:55:59 +0600

    users api added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bkpsd7x.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,8 @@ async function run() {
         await client.connect();
 
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
+        const userCollection = client.db('coffeeDB').collection('user');
+
 
         app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
@@ -77,6 +79,40 @@ async function run() {
             res.send(result);
         })
 
+        // user related API's
+
+        app.get('/users', async(req, res) =>{
+            const cursor = userCollection.find()
+            const users = await cursor.toArray()
+            res.send(users)
+        })
+
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            console.log(user)
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users', async(req, res) =>{
+            const user = req.body
+            const filter = {email: user.email}
+            const updateDoc = {
+                $set: {
+                    lastLoggedAt: user.lastLoggedAt
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id' , async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
